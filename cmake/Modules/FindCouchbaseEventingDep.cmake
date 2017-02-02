a5549f15ae1eba972c45101b7bc2aa7774005157
harsha <hhs.couchbase@gmail.com>
2017-02-03 00:49:35 +0530

    Add eventing project to tlm
    Change-Id: I9f1a13bd68c3728c6a6c505c72434e4664820dc7

---
+++ cmake/Modules/FindCouchbaseEventingDep.cmake
@@ -0,0 +1,60 @@
+FIND_PATH(LIBCOUCHBASE_INCLUDE_DIR n1ql.h
+          PATH_SUFFIXES libcouchbase
+          PATHS
+               ~/Library/Frameworks
+               /Library/Frameworks
+               /opt/local
+               /opt/csw
+               /opt/jemalloc
+               /opt)
+
+
+FIND_LIBRARY( LIBCOUCHBASE_LIBRARIES
+              NAMES couchbase libcoucbase
+             PATH_SUFFIXES lib
+             PATHS
+                 ~/Library/Frameworks
+                 /Library/Frameworks
+                 /opt/local
+                 /opt/csw
+                 /opt/jemalloc
+                 /opt)
+
+MESSAGE(STATUS "Found libcouchbase header and libraries ${LIBCOUCHBASE_INCLUDE_DIR} : ${LIBCOUCHBASE_LIBRARIES}")
+IF (LIBCOUCHBASE_LIBRARIES)
+  MESSAGE(STATUS "Found libcouchbase sdk ${LIBCOUCHBASE_LIBRARIES}")
+ELSE (LIBCOUCHBASE_LIBRARIES)
+  MESSAGE(FATAL_ERROR "Can't build Eventing without libcouchbase sdk")
+ENDIF (LIBCOUCHBASE_LIBRARIES)
+
+FIND_PATH(LIBUV_INCLUDE_DIR uv.h
+          PATH_SUFFIXES libuv
+          PATHS
+               ~/Library/Frameworks
+               /Library/Frameworks
+               /opt/local
+               /opt/csw
+               /opt/jemalloc
+               /opt)
+
+
+FIND_LIBRARY( LIBUV_LIBRARIES
+              NAMES uv libuv
+             PATH_SUFFIXES lib
+             PATHS
+                 ~/Library/Frameworks
+                 /Library/Frameworks
+                 /opt/local
+                 /opt/csw
+                 /opt/jemalloc
+                 /opt)
+
+MESSAGE(STATUS "Found libuv header and libraries ${LIBUV_INCLUDE_DIR} : ${LIBUV_LIBRARIES}")
+IF (LIBUV_LIBRARIES)
+  MESSAGE(STATUS "Found libuv ${LIBUV_LIBRARIES}")
+ELSE (LIBUV_LIBRARIES)
+  MESSAGE(FATAL_ERROR "Can't build Eventing without libuv")
+ENDIF (LIBUV_LIBRARIES)
+
+
+MARK_AS_ADVANCED(LIBCOUCHBASE_INCLUDE_DIR LIBCOUCHBASE_LIBRARIES LIBUV_INCLUDE_DIR LIBUV_LIBRARIES)
